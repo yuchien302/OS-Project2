a72825ac04ee0b423c6b842fdb02ca078367cf60
Raphael <raphaeltnfsh@gmail.com>
2013-05-05 19:22:08 +0800

    update makefile

---
+++ threads/myscheduler.h
@@ -0,0 +1,44 @@
+// myscheduler.h 
+//	Data structures for the thread dispatcher and myscheduler.
+//	Primarily, the list of threads that are ready to run.
+//
+// Copyright (c) 1992-1996 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include "copyright.h"
+#include "list.h"
+#include "thread.h"
+
+// The following class defines the myscheduler/dispatcher abstraction -- 
+// the data structures and operations needed to keep track of which 
+// thread is running, and which threads are ready but not running.
+
+class MyScheduler {
+  public:
+    MyScheduler();		// Initialize list of ready threads 
+    ~MyScheduler();		// De-allocate ready list
+
+    void ReadyToRun(Thread* thread);	
+    				// Thread can be dispatched.
+    Thread* FindNextToRun();	// Dequeue first thread on the ready 
+				// list, if any, and return thread.
+    void Run(Thread* nextThread, bool finishing);
+    				// Cause nextThread to start running
+    void CheckToBeDestroyed();// Check if thread that had been
+    				// running needs to be deleted
+    void Print();		// Print contents of ready list
+    
+    // SelfTest for myscheduler is implemented in class Thread
+    
+  private:
+    List<Thread *> *readyList;  // queue of threads that are ready to run,
+				// but not running
+    Thread *toBeDestroyed;	// finishing thread to be destroyed
+    				// by the next thread that runs
+};
+
+#endif // SCHEDULER_H
