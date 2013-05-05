2088425743be6ce5b38ec3bd7fb90c424e136883
Raphael <raphaeltnfsh@gmail.com>
2013-05-05 21:49:33 +0800

    Implementing priority scheduling

--- threads/myscheduler.cc
+++ threads/myscheduler.cc
@@ -27,7 +27,6 @@
 //      returns 0 if x == y
 //      returns 1 if x > y
 int CompareThreads(Thread* x, Thread* y){
-    cout << x->GetPriority() << " " << y->GetPriority() << endl;
     if( (x->GetPriority()) < (y->GetPriority()) )
         return -1;
     if( (x->GetPriority()) == (y->GetPriority()) )
