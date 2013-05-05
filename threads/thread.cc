5bbd75d12ceb2a7372cfdba992a32fd8c79c0c6a
Raphael <raphaeltnfsh@gmail.com>
2013-05-06 01:50:12 +0800

    Implementing priority scheduling

--- threads/thread.cc
+++ threads/thread.cc
@@ -544,7 +544,7 @@ Thread::MyScheduling(char*ParameterFile)
     for(int i=0; i<total; i++){
         pin >> name >> priority >> times;
         // cout << name << " " << priority << " " << times << endl;
-        t = new Thread( (char*) name.c_str() ); 
+        t = new Thread( name ); 
         // cout << "FUCK" << endl;
         t->SetPriority(priority);
         // t->SetName(name);
